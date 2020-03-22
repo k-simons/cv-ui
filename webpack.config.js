12c7aafa930e6e27cb3fcea7936180fe07ee09d2
Kevin Simons <ksimons@palantir.com>
2020-03-22 16:47:15 -0700

    Some typescript

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,22 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/main.ts',
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
   },
   plugins: [new HtmlWebpackPlugin()],
   devServer: {
