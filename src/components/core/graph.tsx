61f08935666fd605498f5662921f8348ef133bd5
Kevin Simons <ksimons@palantir.com>
2020-04-16 20:33:19 -0700

    CR

--- src/components/core/graph.tsx
+++ src/components/core/graph.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { IDateMap, RowResult } from "../../data-fetching/fetcher";
 import { XAxis, LineChart, BarChart, YAxis, Legend, CartesianGrid, Line, Tooltip, Bar, ItemSorter , TooltipPayload } from "recharts";
-import { Classes, IButtonProps, Checkbox, IPopoverProps, MenuItem, Position } from "@blueprintjs/core";
+import { Classes, IButtonProps, Checkbox, IPopoverProps, MenuItem, Position, NumericInput } from "@blueprintjs/core";
 import { MAX_ITEMS_TO_RENDER, SingleSelect } from "../select/singleSelect/singleSelect";
 import { AriesMultiSelect } from "../select/ariesMultiSelect/ariesMultiSelect";
+import { StatePopulation } from "./stateData";
 
 
 
@@ -14,6 +15,12 @@ interface IProps {
 interface ISingleSeries {
     name: string;
     data: Array<ISingleDate>;
+}
+
+interface IBarSeries {
+    name: string;
+    optionTotal: number;
+    stockTotal: number;
 }
 
 interface ISingleDate {
@@ -72,18 +79,134 @@ const series = [
     activeSeries: KeyOfRowResult
     stateFilter: Array<string>
     showDiff: boolean
+    perPopulation: boolean
     excludeStateFilter: Array<string>
 }
 
 const VizSelect = SingleSelect.ofType<KeyOfRowResult>();
 const StringMultiSelect = AriesMultiSelect.ofType<string>();
 
+const callPriceToPremium: {[date: number]: number; } = {
+    10.00: 21.63,
+    11.00: 20.73,
+    12.00: 19.68,
+    13.00: 18.70,
+    14.00: 17.95,
+    15.00: 17.08,
+    16.00: 16.23,
+    17.00: 15.48,
+    18.00: 14.48,
+    19.00: 13.90,
+    20.00: 12.98,
+    21.00: 11.98,
+    22.00: 11.38,
+    23.00: 10.60,
+    24.00: 9.88 ,
+    25.00: 9.18 ,
+    26.00: 8.48 ,
+    27.00: 8.00 ,
+    28.00: 7.23 ,
+    29.00: 6.65 ,
+    30.00: 6.05 ,
+    31.00: 5.55 ,
+    32.00: 5.05 ,
+    33.00: 4.58 ,
+    34.00: 4.15 ,
+    35.00: 3.63 ,
+    36.00: 3.45 ,
+    37.00: 3.05 ,
+    38.00: 2.74 ,
+    39.00: 2.43 ,
+    40.00: 2.17 ,
+    41.00: 1.94 ,
+    42.00: 1.77 ,
+    43.00: 1.67 ,
+    44.00: 1.52 ,
+    45.00: 1.31 ,
+    46.00: 1.06 ,
+    47.00: 1.10 ,
+    48.00: 0.96 ,
+    49.00: 0.89 ,
+    50.00: 0.75 ,
+    52.50: 0.54 ,
+    55.00: 0.34 ,
+    57.50: 0.21 ,
+    60.00: 0.23 ,
+    62.50: 0.23 ,
+    65.00: 0.15 ,
+    67.50: 0.10 ,
+    70.00: 0.07 ,
+    72.50: 0.10 ,
+    75.00: 0.09 ,
+    77.50: 0.08 ,
+    80.00: 0.07 ,
+    82.50: 0.06 ,
+    85.00: 0.06 ,
+    87.50: 0.05 ,
+    90.00: 0.03 ,
+    92.50: 0.02 ,
+    95.00: 0.02 ,
+    97.50: 0.04 ,
+    100.00: 0.05 ,
+    105.00: 0.05 ,
+    110.00: 0.04 ,
+    115.00: 0.04 ,
+}
+
 export class GraphView extends React.PureComponent<IProps, IState> {
+    public render() {
+        const contractCount = 1
+        const optionsInContract = 100
+        const totalOptions = contractCount * optionsInContract
+        const premium = 3.75
+        const callPrice = 35.00
+        const fixedLoss = contractCount * totalOptions * premium
+        const stockPurchased = 20
+        const currentPrice = 34.10
+        const seriesSet: Array<IBarSeries> = []
+        for (let step = 24; step < 44; step++) {
+            const diffInOrder = (step - callPrice) * totalOptions;
+            const totalDiff = diffInOrder - fixedLoss;
+            const optionTotal = Math.max(-1 * fixedLoss, totalDiff)
+            const stockTotal = (step - currentPrice) * stockPurchased
+            seriesSet.push(
+                {
+                    optionTotal: optionTotal,
+                    name: step + "",
+                    stockTotal: stockTotal
+                })
+        }
+        return (
+            <div>
+                <NumericInput>
+
+                </NumericInput>
+            <BarChart
+                width={1300}
+                height={800}
+                data={seriesSet}
+                margin={{
+                top: 5, right: 30, left: 20, bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Bar dataKey="optionTotal" fill="#8884d8" />
+                <Bar dataKey="stockTotal" fill="#82ca9d" />
+            </BarChart>
+            </div>
+        )
+    }
+}
+export class GraphView2 extends React.PureComponent<IProps, IState> {
 
     public state: IState = {
         activeSeries: "newCases",
         excludeStateFilter: [],
-        showDiff: true,
+        showDiff: false,
+        perPopulation: false,
         stateFilter: []
     };
 
@@ -166,6 +289,10 @@ export class GraphView extends React.PureComponent<IProps, IState> {
                         checked={this.state.showDiff}
                         onChange={this.onCheckChange}
                     />
+                    <Checkbox
+                        checked={this.state.perPopulation}
+                        onChange={this.onCheckChangePopulation}
+                    />
                 </div>
                 <LineChart width={1000} height={600}>
                     <CartesianGrid strokeDasharray="3 3" />
@@ -184,6 +311,10 @@ export class GraphView extends React.PureComponent<IProps, IState> {
 
     private onCheckChange = () => {
         this.setState({showDiff: !this.state.showDiff})
+    }
+
+    private onCheckChangePopulation = () => {
+        this.setState({perPopulation: !this.state.perPopulation})
     }
 
     private getFill(index: number): string {
@@ -250,8 +381,45 @@ export class GraphView extends React.PureComponent<IProps, IState> {
                 filteredDateMap[date] = filtered
             }
         })
-        console.log(filteredDateMap)
-        return filteredDateMap
+        const copied = JSON.parse(JSON.stringify(filteredDateMap)) as IDateMap
+        console.log(this.state.perPopulation)
+        if (this.state.showDiff) {
+            const keys = Object.keys(filteredDateMap).sort()
+            keys.forEach((key, i) =>  {
+                if ((i + 1) == keys.length) {
+                    delete copied[key];
+                    return
+                }
+                if (i == 0) {
+                    copied[key].forEach(r => {
+                        r[this.state.activeSeries] = 0
+                    })
+                    return;
+                } else {
+                    copied[key].forEach(r => {
+                        filteredDateMap[keys[i-1]].forEach(rPrime => {
+                            if (r.state == rPrime.state) {
+                                r[this.state.activeSeries] = r[this.state.activeSeries] - rPrime[this.state.activeSeries]
+                            }
+                        })
+                    })
+                }
+            })
+        } else if (this.state.perPopulation) {
+            Object.keys(copied).sort().forEach(key =>  {
+                copied[key].forEach(r => {
+                    const found = StatePopulation.find(s => s.State == r.state)
+                    if (found == null) {
+                        console.log("NOT FOUND")
+                        console.log(r)
+                        r[this.state.activeSeries] = 0
+                    } else {
+                        r[this.state.activeSeries] = r[this.state.activeSeries] / found.Pop
+                    }
+                })
+            })
+        }
+        return copied
     }
 
     private passedFilter(rowResult: RowResult): boolean {
